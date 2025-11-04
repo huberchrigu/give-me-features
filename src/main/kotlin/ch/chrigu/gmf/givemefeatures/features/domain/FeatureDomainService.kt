a324f40db6901017323b15761ce0b9462a9414b5
Christoph Huber <huber@silberruecken.ch>
2025-11-04 15:57:05 +0100

    Simplified merging

--- src/main/kotlin/ch/chrigu/gmf/givemefeatures/features/domain/FeatureDomainService.kt
+++ src/main/kotlin/ch/chrigu/gmf/givemefeatures/features/domain/FeatureDomainService.kt
@@ -5,7 +5,6 @@ import ch.chrigu.gmf.givemefeatures.features.FeatureId
 import ch.chrigu.gmf.givemefeatures.features.repository.FeatureRepository
 import ch.chrigu.gmf.givemefeatures.shared.history.HistoryMerger
 import ch.chrigu.gmf.givemefeatures.shared.markdown.Markdown
-import ch.chrigu.gmf.givemefeatures.shared.markdown.MarkdownDiff
 import org.springframework.stereotype.Component
 
 @Component
@@ -13,14 +12,8 @@ class FeatureDomainService(featureRepository: FeatureRepository) {
     private val historyMerger = HistoryMerger(featureRepository)
 
     suspend fun mergeWithVersion(id: FeatureId, newName: String, newDescription: Markdown, baseVersion: Long, mergeWithVersion: Long): Feature {
-        return historyMerger.mergeWithVersion(id, baseVersion, mergeWithVersion) { base, theirs ->
-            val mergedName = MarkdownDiff.simpleMerge(base.name, newName, theirs.name)
-            val mergedDescription = MarkdownDiff.merge3(
-                base.description,
-                newDescription,
-                theirs.description
-            )
-            Feature(id, mergedName, mergedDescription, theirs.tasks, theirs.version)
+        return historyMerger.mergeWithVersion(id, baseVersion, mergeWithVersion) {
+            Feature(id, newName.merge { name }, newDescription.merge { description }, theirs.tasks, theirs.version)
         }
     }
 }
