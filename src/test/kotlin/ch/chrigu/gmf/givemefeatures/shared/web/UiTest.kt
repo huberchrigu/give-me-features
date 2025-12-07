5adcb41dbdff6e706b21b8197d84d98731d604af
Christoph Huber <huber@silberruecken.ch>
2025-12-07 21:30:28 +0100

    @SpringBootConfiguration breaks tests that do not include it

--- src/test/kotlin/ch/chrigu/gmf/givemefeatures/shared/web/UiTest.kt
+++ src/test/kotlin/ch/chrigu/gmf/givemefeatures/shared/web/UiTest.kt
@@ -8,9 +8,9 @@ import org.springframework.core.annotation.AliasFor
 import org.springframework.test.context.TestConstructor
 import kotlin.reflect.KClass
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = [WebFluxTestConfig::class])
-@Import
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(WebFluxTestConfig::class)
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ExtendWith(SaveArtifactsOnFailure::class)
-annotation class UiTest(@get:AliasFor(annotation = Import::class) val value: KClass<*>)
+annotation class UiTest(@get:AliasFor(annotation = SpringBootTest::class, attribute = "classes") val value: KClass<*>)
--- src/test/kotlin/ch/chrigu/gmf/givemefeatures/shared/web/WebFluxTestConfig.kt
+++ src/test/kotlin/ch/chrigu/gmf/givemefeatures/shared/web/WebFluxTestConfig.kt
@@ -3,12 +3,12 @@ package ch.chrigu.gmf.givemefeatures.shared.web
 import ch.chrigu.gmf.givemefeatures.shared.security.SecurityConfiguration
 import ch.chrigu.gmf.givemefeatures.shared.web.advices.GlobalControllerAdvice
 import ch.chrigu.gmf.givemefeatures.shared.web.advices.GlobalExceptionHandler
-import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.data.mongodb.autoconfigure.DataMongoReactiveRepositoriesAutoConfiguration
+import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Import
 
-@SpringBootConfiguration(proxyBeanMethods = false)
+@TestConfiguration(proxyBeanMethods = false)
 @EnableAutoConfiguration(
     exclude = [DataMongoReactiveRepositoriesAutoConfiguration::class],
     excludeName = [MODULITH_AUTO_CONFIG, MODULE_OBSERVABILITY_AUTO_CONFIG]
