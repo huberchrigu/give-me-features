b5a416e49698e87d8f5f4054457a5218a3f44708
Christoph Huber <huber@silberruecken.ch>
2026-01-23 08:45:23 +0100

    Fixed template

--- src/main/kotlin/ch/chrigu/gmf/plugins/PluginService.kt
+++ src/main/kotlin/ch/chrigu/gmf/plugins/PluginService.kt
@@ -5,18 +5,21 @@ import ch.chrigu.gmf.plugins.mongo.PluginStatusRepository
 import jakarta.annotation.PostConstruct
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.runBlocking
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Service
 
 @Service
 class PluginService(private val pluginStatusRepository: PluginStatusRepository, private val plugins: List<Plugin> = emptyList()) {
     @PostConstruct
-    suspend fun init() {
-        val current = pluginStatusRepository.findAll().toList()
-        val new = plugins - current
-        val removed = current - plugins
-        pluginStatusRepository.saveAll(new.map { PluginStatus.from(it) }).toList()
-        pluginStatusRepository.deleteAll(removed)
+    fun init() {
+        runBlocking {
+            val current = pluginStatusRepository.findAll().toList()
+            val new = plugins minusPluginStatus current
+            val removed = current minusPlugins plugins
+            pluginStatusRepository.saveAll(new.map { PluginStatus.from(it) }).toList()
+            pluginStatusRepository.deleteAll(removed)
+        }
     }
 
     @PreAuthorize("hasRole('ADMIN')")
@@ -32,12 +35,12 @@ class PluginService(private val pluginStatusRepository: PluginStatusRepository, 
         pluginStatusRepository.save(it)
     }
 
-    private operator fun List<Plugin>.minus(other: List<PluginStatus>): List<Plugin> {
+    private infix fun List<Plugin>.minusPluginStatus(other: List<PluginStatus>): List<Plugin> {
         val otherIds = other.map { it.pluginId }
         return filter { !otherIds.contains(it.id) }
     }
 
-    private operator fun List<PluginStatus>.minus(other: List<Plugin>): List<PluginStatus> {
+    private infix fun List<PluginStatus>.minusPlugins(other: List<Plugin>): List<PluginStatus> {
         val otherIds = other.map { it.id }
         return filter { !otherIds.contains(it.pluginId) }
     }
--- testing-plugin/src/main/kotlin/ch/chrigu/gmf/testing/TestingAutoConfiguration.kt
+++ testing-plugin/src/main/kotlin/ch/chrigu/gmf/testing/TestingAutoConfiguration.kt
@@ -1,5 +1,7 @@
 package ch.chrigu.gmf.testing
 
+import org.springframework.boot.autoconfigure.AutoConfiguration
+
 @AutoConfiguration
 class TestingAutoConfiguration {
 }
